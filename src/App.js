0db0d7baa13fa9f3d5d54ab55b28fc37819aaebc
aikchun <aikchun616@gmail.com>
2018-09-04 14:31:38 +0800

    Implemented state on the app level. Using the state we control whether to show the SideDrawer and Backdrop

--- src/App.js
+++ src/App.js
@@ -5,12 +5,28 @@ import SideDrawer from './components/SideDrawer/SideDrawer';
 import Backdrop from './components/Backdrop/Backdrop';
 
 class App extends Component {
+  state = {
+    sideDrawerOpen: false
+  };
+
+  drawerToggleClickHandler = () => {
+    this.setState(prevState => {
+      return { sideDrawerOpen: !prevState.sideDrawerOpen }
+    });
+  };
+
   render() {
+    let sideDrawer;
+    let backdrop;
+    if (this.state.sideDrawerOpen) {
+      sideDrawer = <SideDrawer />
+      backdrop = <Backdrop />
+    }
     return (
       <div className="App" style={{ height: '100%' }}>
-        <Toolbar />
-        <SideDrawer />
-        <Backdrop />
+        <Toolbar drawerClickHandler={this.drawerToggleClickHandler}/>
+        { sideDrawer }
+        { backdrop }
         <main style={{ marginTop: '64px' }}>
           <p>This is the page content!</p>
 
