243fdf88542a5823cf0e945b084609334975491d
aikchun <aikchun616@gmail.com>
2018-09-04 14:44:39 +0800

    Added css transform and transition for SideDrawer animation

--- src/App.js
+++ src/App.js
@@ -20,16 +20,16 @@ class App extends Component {
   }
 
   render() {
-    let sideDrawer;
     let backdrop;
+
     if (this.state.sideDrawerOpen) {
-      sideDrawer = <SideDrawer />
       backdrop = <Backdrop click={ this.backdropClickHandler }/>
     }
+
     return (
       <div className="App" style={{ height: '100%' }}>
         <Toolbar drawerClickHandler={this.drawerToggleClickHandler}/>
-        { sideDrawer }
+        <SideDrawer show={ this.state.sideDrawerOpen }/>
         { backdrop }
         <main style={{ marginTop: '64px' }}>
           <p>This is the page content!</p>
